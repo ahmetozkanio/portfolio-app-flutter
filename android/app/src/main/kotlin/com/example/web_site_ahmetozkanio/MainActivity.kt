eee6c7c708600acb86b0fe78e6322bb0845b8ab0
ahmetozkanio <bledwarrior@gmail.com>
2022-10-08 17:48:56 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/web_site_ahmetozkanio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.web_site_ahmetozkanio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:web_site_ahmetozkanio/src/home/home.dart';
+
+void main() {
+  runApp(const Home());
+}
