1048cf82fa5224f2ce71d2b5d626522a960c0165
ahmetozkanio <bledwarrior@gmail.com>
2022-10-27 14:10:30 +0300

    projects detail page

--- lib/src/home/home.dart
+++ lib/src/home/home.dart
@@ -5,6 +5,7 @@ import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:web_site_ahmetozkanio/src/home/home_view.dart';
 import 'package:web_site_ahmetozkanio/src/home/home_view_controller.dart';
 import 'package:web_site_ahmetozkanio/src/projects/project_detail/project_detail_view.dart';
+import 'package:web_site_ahmetozkanio/src/projects/project_detail/project_detail_view_controller.dart';
 import 'package:web_site_ahmetozkanio/src/projects/projects_view_controller.dart';
 import 'package:web_site_ahmetozkanio/src/theme/panachefile/green_theme.dart';
 import 'package:web_site_ahmetozkanio/src/theme/panachefile/grey_theme.dart';
@@ -35,8 +36,10 @@ class Home extends StatelessWidget {
           // }),
         ),
         GetPage(
-          name: '/project/:name',
+          name: '/project/:slug',
+
           page: () => ProjectDetailView(),
+          // binding: () => Get.lazyPut<ProjectDetailViewController>(() => ProjectDetailViewController())
         ),
       ],
       debugShowCheckedModeBanner: false,
